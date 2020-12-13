70478a82eef688fe7a86f0b414206e31ab929d35
taekvan <taekvandovan@gmail.com>
2020-12-13 19:13:48 +0300

    Adding login page checking

---
+++ pages/login_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link.click()
+        a = "login" in str(self.browser.current_url)
+        assert a, "This s not a login page"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
